2cb63a897118470f9f829924e118792ac32b8bca
Songklod <itoon.sit@gmail.com>
2023-09-30 15:07:23 +0700

    feat: add Nuxt Icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-headlessui', '@vueuse/nuxt']
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-headlessui', '@vueuse/nuxt', 'nuxt-icon']
 })
