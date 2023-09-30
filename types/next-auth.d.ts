6923b3ce3c7dacba08c9aaff17fa2a009e0be1e9
Songklod <itoon.sit@gmail.com>
2023-10-01 00:56:02 +0700

    chore: add comment for nuxt auth custom attributes type

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,9 +2,10 @@ declare module 'next-auth' {
   interface Session {
     user?: {
       id?: string | null
-      username?: string | null
       name?: string | null
       email?: string | null
+      // custom attributes
+      username?: string | null
     }
     expires: Date
   }
